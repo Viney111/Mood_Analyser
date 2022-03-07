86374a788ffe8daf32452d84dc324f50e66fbe22
Viney <vineykhaneja999@gmail.com>
2022-03-07 13:38:49 +0530

    [Viney] FEATURE ADDED: Reflection Usage for Invoking Mood Analyse Method

--- Mood_Analyser_Problem/MoodAnalyserReflector.cs
+++ Mood_Analyser_Problem/MoodAnalyserReflector.cs
@@ -8,8 +8,8 @@ using System.Text.RegularExpressions;
 
 namespace Mood_Analyser_Problem
 {
-    
-    public class MoodAnalyserFactory
+
+    public class MoodAnalyserReflector
     {
         #region Method for Creating object of Parameterized Constructor
         public static object CreateMoodAnalyserObjectParameterizedConstructor(string className, string Constructor, string message)
@@ -22,7 +22,7 @@ namespace Mood_Analyser_Problem
                     Assembly assembly = Assembly.GetExecutingAssembly();
                     Type moodAnalyserType = assembly.GetType(className);
                     object[] args = { message };
-                    object resultant = Activator.CreateInstance(moodAnalyserType,args);
+                    object resultant = Activator.CreateInstance(moodAnalyserType, args);
                     return resultant;
                 }
                 catch
@@ -62,5 +62,26 @@ namespace Mood_Analyser_Problem
             }
         }
         #endregion
+
+        #region Method for Invoking Mood Analyse Method
+        public static string InvokeMoodAnalyseMethod(string methodName, string message)
+        {
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Type moodAnalyseType = assembly.GetType("Mood_Analyser_Problem.MoodAnalyser");
+                object moodAnalyseObjectofParameterlessConstructor = Activator.CreateInstance(moodAnalyseType);
+                MethodInfo gettingMoodAnalyseMethod = moodAnalyseType.GetMethod(methodName);
+                object[] args = { message };
+                string resultant = (string)gettingMoodAnalyseMethod.Invoke(moodAnalyseObjectofParameterlessConstructor, args);
+                return resultant;
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalysisException(ExceptionType.NO_SUCH_METHOD_ERROR, "No such method is present in this class");
+            }
+        }
+        #endregion
     }
 }
+
